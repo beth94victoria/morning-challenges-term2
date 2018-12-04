db94fc33849c1debdd5f0797100d6324e68cdfc6
beth94victoria <beth.callanan@gmail.com>
2018-12-04 14:29:06 +1000

    finished testing for 11_binary_to_decimal

--- 11_binary_to_decimal.js
+++ 11_binary_to_decimal.js
@@ -22,17 +22,3 @@ function binaryToDecimal(binaryNumber) {
 
 }
 module.exports = binaryToDecimal
-
-// let assert = require('assert')
-
-// describe('Binary to decimal', function () {
-//     it('Should convert a binary to its decimal value', function () {
-//         assert.equal((9), binaryToDecimal(1001))
-//         assert.equal((15), binaryToDecimal(1111))
-//         assert.equal((31), binaryToDecimal(11111))
-//     })
-//     it('Should return a null if the binary has a number other than 0 or 1', function () {
-//         assert.equal((null), binaryToDecimal(1002))
-//         assert.equal((null), binaryToDecimal(1220))
-//     })
-// })
--- test/11_binary_to_decimal.js
+++ test/11_binary_to_decimal.js
@@ -7,13 +7,20 @@ describe('Binary to decimal', function () {
             assert.equal((9), binaryToDecimal(1001))
         })
     })
-    it('Should convert a binary to its decimal value', function () {
-        assert.equal((9), binaryToDecimal(1001))
-        assert.equal((15), binaryToDecimal(1111))
-        assert.equal((31), binaryToDecimal(11111))
+    context('Should convert a binary to its decimal value', function () {
+        it('should equal 15 if binary number is 1111', function () {
+            assert.equal((15), binaryToDecimal(1111))
+        })
+        it('should equal 31 if binar number is 11111', function () {
+            assert.equal((31), binaryToDecimal(11111))
+        })
     })
-    it('Should return a null if the binary has a number other than 0 or 1', function () {
-        assert.equal((null), binaryToDecimal(1002))
-        assert.equal((null), binaryToDecimal(1220))
+    context('Should return a null if the binary has a number other than 0 or 1', function () {
+        it('should return null if binary number is 1002', function () {
+            assert.equal((null), binaryToDecimal(1002))
+        })
+        it('should return null if binary number is 1220', function () {
+            assert.equal((null), binaryToDecimal(1220))
+        })
     })
 })
